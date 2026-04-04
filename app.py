2bbd6ca59001160f449aee10f0ef55c9aeaae44f
indicar <indicar@users.noreply.github.com>
2026-04-04 13:32:59 +0300

    websocket

--- app.py
+++ app.py
@@ -1157,4 +1157,4 @@ if __name__ == '__main__':
     except ImportError:
         pass
     
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+    socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
